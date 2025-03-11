b087bffc67561ac85f1e40efb9c9db362fe357a4
Rushi Parikh <126984217+Rushi1508@users.noreply.github.com>
2025-03-10 21:14:58 -0700

    Delete gold directory

--- gold/models/vehicle_health/vehicle_health_analysis.sql
+++
@@ -1,9 +0,0 @@
-{% set nessie_branch = var('nessie_branch', 'main') %}
-
-SELECT
-    VehicleID,
-    Model,
-    ManufacturingYear,
-    Alerts,
-    MaintenanceHistory
-FROM {{ source('silver', 'vehicle_health_logs') }} AT branch {{ nessie_branch }}
