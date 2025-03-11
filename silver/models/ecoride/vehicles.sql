42ab0208e00040516b71107c3f6254ac90d2421a
Rushi Parikh <126984217+Rushi1508@users.noreply.github.com>
2025-03-10 21:15:11 -0700

    Delete silver directory

--- silver/models/ecoride/vehicles.sql
+++
@@ -1,9 +0,0 @@
-{{ config( twin_strategy="allow", materialized="table" ) }}
-
-SELECT
-    id,
-    model_name,
-    model_type,
-    color,
-    "year"
-FROM {{ source("ecoride_bronze", "vehicles") }}
--- silver/models/vehicle_health/vehicle_health_logs.sql
+++
@@ -1,9 +0,0 @@
-{{ config( twin_strategy="allow", materialized="table" ) }}
-
-SELECT
-    VehicleID,
-    Model,
-    ManufacturingYear,
-    Alerts,
-    MaintenanceHistory
-FROM {{ source("vehicle_health_bronze", "logs") }}
